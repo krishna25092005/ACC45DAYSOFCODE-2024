b3d3eae1dbf58b61c6f217b05d626602c388bb63
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-10-02 10:27:11 +0530

    Day 4 - Easy, Medium, Hard

---
+++ Day 4/The Three Topics.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int a, b, c, x;
+    cin >> a >> b >> c >> x;
+    if (x == a || x == b || x == c)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
