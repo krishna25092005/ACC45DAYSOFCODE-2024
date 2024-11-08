bfe9de9e2def7797951f589c7f796dd4fce01621
Krishna Chauhan <145191713+krishna25092005@users.noreply.github.com>
2024-11-08 21:00:01 +0530

    Day 37 - Easy, Medium, Hard

---
+++ Day 37/Magician versus Chef.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,s;
+	    cin>>n>>x>>s;
+	    for(int i=1;i<=s;i++){
+	        int a,b;
+	        cin>>a>>b;
+	        if(b==x) x=a;
+	        else if(a==x) x=b;
+	        
+	    }
+	    cout<<x<<endl;
+	}
+	return 0;
+}
